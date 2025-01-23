cdee2f3accfedd7ba0048b15624081677563493b
James Wolff <james.wolff0@gmail.com>
2025-01-22 20:16:27 -0600

    Move bot_base script

--- PestWars/scripts/bot_base.gd
+++ PestWars/scripts/bot_base.gd
@@ -1,7 +1,11 @@
 extends StaticBody3D
 
+@export var spawn_rate: float = 1.0
+
 @onready var path = $Path3D
+
 var path_followers = []
+var spawn_speed_multiplier = 1.0
 
 
 func add_path_follower(node: Node3D):
@@ -12,3 +16,7 @@ func add_path_follower(node: Node3D):
 	path_followers.append(path_follower)
 	path.add_child(path_follower)
 	return path_follower
+
+
+func set_spawn_speed_multiplier(multiplier: float) -> void:
+	spawn_speed_multiplier = multiplier
