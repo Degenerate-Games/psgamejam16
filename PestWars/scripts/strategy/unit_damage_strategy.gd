467a8a8f84f42ab3ced63e6e7ba90c253b74d154
James Wolff <james.wolff0@gmail.com>
2025-01-23 21:23:00 -0600

    Update damage strategy to effect HurtboxComponent

--- PestWars/scripts/strategy/unit_damage_strategy.gd
+++ PestWars/scripts/strategy/unit_damage_strategy.gd
@@ -5,5 +5,6 @@ class_name UnitDamageStrategy
 @export_range(-100, 100, 1) var damage_multiplier: float = 1.0
 
 func apply_upgrade(unit: PhysicsBody3D) -> void:
-	if unit.get_groups().has("unit"):
-		unit.set_damage_multiplier(damage_multiplier)
+	if unit.has_node("HurtboxComponent"):
+		var hurtbox: HurtboxComponent = unit.get_node("HurtboxComponent")
+		hurtbox.set_damage_multiplier(damage_multiplier)
