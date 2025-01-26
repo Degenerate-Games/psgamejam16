ebf94a33600c80342760254b738689cca989db8e
James Wolff <james.wolff0@gmail.com>
2025-01-25 18:18:42 -0600

    Remove strategies

--- PestWars/scripts/strategy/unit_speed_strategy.gd
+++
@@ -1,9 +0,0 @@
-extends BaseStrategy
-class_name UnitSpeedStrategy
-
-
-@export_range(-100, 100, 1) var speed_multiplier: float = 1.0
-
-func apply_upgrade(unit: PhysicsBody3D) -> void:
-	if unit.get_groups().has("unit"):
-		unit.set_speed_multiplier(speed_multiplier)
