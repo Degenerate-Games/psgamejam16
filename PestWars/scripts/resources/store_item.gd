d24282342017338b2afec135985f6675dd0c20e3
James Wolff <james.wolff0@gmail.com>
2025-01-25 18:19:01 -0600

    Add store_item and upgrade resources

---
+++ PestWars/scripts/resources/store_item.gd
@@ -0,0 +1,28 @@
+extends Resource
+class_name StoreItem
+
+@export var item: Upgrade
+@export var base_cost: int
+@export var cost_multiplier: float
+
+var item_name_button: Button
+var item_level_label: Label
+var item_cost_label: Label
+
+func get_cost() -> int:
+	return get_cost_at_level(item.current_level)
+
+
+func get_cost_at_level(level: int) -> int:
+	if level <= 0 or level > item.max_level:
+		return -1
+	else:
+		return round(base_cost * pow(cost_multiplier, level - 1))
+
+
+func buy() -> void:
+	if item.upgrade():
+		item_level_label.text = str(item.current_level)
+		item_cost_label.text = str(get_cost())
+	else:
+		item_name_button.disabled = true
---
+++ PestWars/scripts/resources/upgrade.gd
@@ -0,0 +1,38 @@
+extends Resource
+class_name Upgrade
+
+## The name of the upgrade.
+@export var name: String
+## The maximum level of the upgrade.
+@export var max_level: int
+## Whether to use advanced scaling for the upgrade. If checked the advanced_scaling array will be used. Otherwise, the level will be used as the scaling modifier.
+@export var use_advanced_scaling: bool = false
+## The advanced scaling array for the upgrade. The size of the array must be the same as the max level of the upgrade if use advanced scaling is checked.
+@export var advanced_scaling: Array[float]
+
+
+var current_level: int = 1
+
+
+func _ready():
+	if use_advanced_scaling:
+		assert(advanced_scaling.size() == max_level, "Advanced scaling array must have the same size as the max level of the upgrade.")
+
+
+func get_scaling() -> float:
+	return get_scaling_at_level(current_level)
+
+
+func get_scaling_at_level(level: int) -> float:
+	if use_advanced_scaling:
+		return advanced_scaling[level - 1]
+	else:
+		return level
+
+
+func upgrade() -> bool:
+	if current_level < max_level:
+		current_level += 1
+		return true
+	else:
+		return false
