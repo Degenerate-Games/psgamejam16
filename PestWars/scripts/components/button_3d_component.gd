576cbb30871b6ddc4eea4a6760cfe50dde79465d
James Wolff <james.wolff0@gmail.com>
2025-01-25 23:48:14 -0600

    Remove redundant signal connections from 3d button

--- PestWars/scripts/components/button_3d_component.gd
+++ PestWars/scripts/components/button_3d_component.gd
@@ -5,10 +5,6 @@ class_name Button3DComponent
 
 signal pressed
 
-func _ready() -> void:
-	connect("input_event", _on_button_3d_input_event)
-	button.connect("pressed", _on_button_pressed)
-
 func _on_button_3d_input_event(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
