15a1c1e266c98979dfa09f5f128668d1e196b2d9
James Wolff <james.wolff0@gmail.com>
2025-01-25 18:16:06 -0600

    Implement hurtbox component

--- PestWars/scripts/components/hurtbox_component.gd
+++ PestWars/scripts/components/hurtbox_component.gd
@@ -2,24 +2,43 @@ extends Area3D
 class_name HurtboxComponent
 
 
-@export var damage: float = 1.0
+@export var base_damage: float = 1.0
 @export var damage_multiplier: float = 1.0
 @export var hits_allowed: int = 1
+@export var damage_interval: float = 0.5
+
+var can_damage: bool = true
+
+@onready var damage_timer: Timer = $Timer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	damage_timer.wait_time = damage_interval
 
 
 func set_damage_multiplier(multiplier: float) -> void:
 	damage_multiplier = multiplier
 
 
+func get_damage() -> float:
+	return base_damage * damage_multiplier
+
+
 func _on_area_entered(area:Area3D) -> void:
-	print("HurtboxComponent: Area entered")
-	if area is HitboxComponent:
-		area.damage(damage * damage_multiplier)
-		hits_allowed -= 1
-		if hits_allowed <= 0:
-			get_parent().queue_free()
+	if !can_damage:
+		return
+
+	if !(area is HitboxComponent):
+		return
+	
+	area.damage(get_damage())
+	hits_allowed -= 1
+	if hits_allowed <= 0:
+		get_parent().queue_free()
+	else:
+		can_damage = false
+		damage_timer.start()
+
+
+func _on_timer_timeout() -> void:
+	can_damage = true
