136ee5d251f25840167a52ce5eb6f2349c11f4de
James Wolff <james.wolff0@gmail.com>
2025-01-25 23:46:08 -0600

    Change pest to work with component system

--- PestWars/scripts/pestBug.gd
+++ PestWars/scripts/pestBug.gd
@@ -1,43 +1,62 @@
 extends CharacterBody3D
 
-@export var MAX_SPEED = 10
 @export var GRAVITY = -9.8
 @export var TARGET_NODE: Node3D
+@export var speed_component: SpeedComponent
 
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
-var speed_multiplier: float = 1.0
+var MODE: int
+
+var MODE_TRACKING = 0
+var MODE_FOLLOWING = 1
+var MODE_IDLE = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	navigation_agent.target_position = TARGET_NODE.global_transform.origin
-	pass # Replace with function body.
+	if TARGET_NODE == null:
+		MODE = MODE_FOLLOWING
+	else:
+		navigation_agent.target_position = TARGET_NODE.global_transform.origin
+		MODE = MODE_TRACKING
+
+
+func _process(_delta) -> void:
+	if MODE == MODE_FOLLOWING:
+		if TARGET_NODE != null and TARGET_NODE.is_inside_tree():
+			MODE = MODE_TRACKING
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if NavigationServer3D.map_get_iteration_id(navigation_agent.get_navigation_map()) == 0:
-		return
-	var new_velocity = Vector3.ZERO
-	if !navigation_agent.is_navigation_finished():
-		if !navigation_agent.is_target_reachable():
-			navigation_agent.target_position = TARGET_NODE.global_transform.origin
-		var next_position = navigation_agent.get_next_path_position()
-		var direction = next_position - global_transform.origin
-		direction = direction.normalized()
-		new_velocity = direction * MAX_SPEED * speed_multiplier
-	if !is_on_floor():
-		new_velocity.y = GRAVITY
-	velocity = velocity.move_toward(new_velocity, 10 * delta)
-	if velocity.length() > 0:
-		var look_at_direction = velocity.normalized()
-		look_at(global_transform.origin + look_at_direction)
-	move_and_slide()
+	if MODE == MODE_TRACKING:
+		if NavigationServer3D.map_get_iteration_id(navigation_agent.get_navigation_map()) == 0:
+			return
+		var new_velocity = Vector3.ZERO
+		if !navigation_agent.is_navigation_finished():
+			if !navigation_agent.is_target_reachable():
+				navigation_agent.target_position = TARGET_NODE.global_transform.origin
+			var next_position = navigation_agent.get_next_path_position()
+			var direction = next_position - global_transform.origin
+			direction = direction.normalized()
+			new_velocity = direction * speed_component.get_max_speed()
+		if !is_on_floor():
+			new_velocity.y = GRAVITY
+		velocity = velocity.move_toward(new_velocity, 50 * delta)
+		if velocity.length() > 0:
+			var look_at_direction = velocity.normalized()
+			look_at(global_transform.origin + look_at_direction)
+		move_and_slide()
+	elif MODE == MODE_FOLLOWING:
+		TARGET_NODE = null
+		velocity = Vector3.ZERO
+		get_parent().progress_ratio += 0.001
 
 
 func _on_navigation_agent_3d_navigation_finished():
-	pass
+	if MODE != MODE_TRACKING:
+		return
 
-
-func set_speed_multiplier(multiplier: float) -> void:
-	speed_multiplier = multiplier
+	if TARGET_NODE.get_groups().find("enemy_base") != -1:
+		TARGET_NODE.call_deferred("add_path_follower", self)
+		MODE = MODE_FOLLOWING
