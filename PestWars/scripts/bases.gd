5beb35fadd4199b59a8a8fb475b936046152024b
James Wolff <james.wolff0@gmail.com>
2025-01-28 21:08:34 -0600

    Begin work on switching bases on destruction

--- PestWars/scripts/bases.gd
+++ PestWars/scripts/bases.gd
@@ -2,13 +2,22 @@ extends Node3D
 
 enum DragMode { LEFT, RIGHT }
 
-## An array of all the base locations in the scene. The first base in the array is the player's base, the last base is the enemy's base. The rest of the bases are neutral.
+## An array of all the base locations in the scene. By convention the first base in the array is the player's base, the last base is the enemy's base and the rest of the bases are neutral.
 @export var base_locations: Array[StaticBody3D] = []
 @export var units_parent: Node3D = self
 
 var drag_start_base: Node3D
 var drag_end_base: Node3D
 var current_drag_mode: DragMode
+
+var bot_base_scene: PackedScene = preload("res://scenes/structures/bot_base.tscn")
+var enemy_base_scene: PackedScene = preload("res://scenes/structures/enemy_base.tscn")
+var neutral_base_scene: PackedScene = preload("res://scenes/structures/neutral_base.tscn")
+
+
+func _ready() -> void:
+	for base in base_locations:
+		base.connect("base_destroyed", _on_base_destroyed)
 
 
 func start_drag(drag_base: Node3D, mode: DragMode) -> void:
@@ -37,3 +46,20 @@ func find_closest_base(unit_position: Vector3, base_group: String) -> Node3D:
 			closest_base = base
 			closest_distance = distance
 	return closest_base
+
+
+func _on_base_destroyed(base: Node3D) -> void:
+	print("Base Destroyed")
+	var base_index = base_locations.find(base)  # Is this getting called?
+	if base_index != -1:
+		if base.get_groups().has("neutral_base"):
+			# TODO: Change to attackers base
+			var new_base = neutral_base_scene.instantiate()
+			new_base.global_transform.origin = base.global_transform.origin
+			base_locations[base_index].queue_free()
+			base_locations[base_index] = new_base
+		else:
+			var new_base = neutral_base_scene.instantiate()
+			new_base.global_transform.origin = base.global_transform.origin
+			base_locations[base_index].queue_free()
+			base_locations[base_index] = new_base
