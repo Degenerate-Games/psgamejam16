16ced99fab66a77b4b059de6c2e207b0aeaf8731
James Wolff <james.wolff0@gmail.com>
2025-01-28 19:36:26 -0600

    Add method to bases script to find the nearest base in a specific group

--- PestWars/scripts/bases.gd
+++ PestWars/scripts/bases.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 enum DragMode { LEFT, RIGHT }
 
+## An array of all the base locations in the scene. The first base in the array is the player's base, the last base is the enemy's base. The rest of the bases are neutral.
 @export var base_locations: Array[StaticBody3D] = []
 @export var units_parent: Node3D = self
 
@@ -25,3 +26,14 @@ func end_drag(drag_base: Node3D) -> void:
 		drag_start_base = null
 		drag_end_base = null
 		current_drag_mode = DragMode.LEFT
+
+
+func find_closest_base(unit_position: Vector3, base_group: String) -> Node3D:
+	var closest_base: Node3D = null
+	var closest_distance = 1e6
+	for base in get_tree().get_nodes_in_group(base_group):
+		var distance = unit_position.distance_to(base.global_transform.origin)
+		if distance < closest_distance:
+			closest_base = base
+			closest_distance = distance
+	return closest_base
